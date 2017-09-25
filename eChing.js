177505eb481aeb2b9f6ada40fb586c205104ce63
0xb100d <25261452+0xb100d@users.noreply.github.com>
2017-09-24 22:33:02 -0700

    Update eChing.js
    Changed values from 0-3 to 6-9 to keep it more traditional.

--- eChing.js
+++ eChing.js
@@ -2,7 +2,7 @@ pragma solidity ^0.4.15;
 
 contract IChing {
 	// takes a question of user and returns array of 6 numbers from 0 to 3
-	// 0 - Old yin, 1 - Young yin, 2 - Old yang, 3 - Young yang
+	// 6 - Old yin, 8 - Young yin, 9 - Old yang, 7 - Young yang
 	function cast(string _question) public constant returns(uint[]) {
 		uint[] memory lines = new uint[](6);
 		for (uint i = 0; i < 6; i++) {
@@ -18,13 +18,13 @@ contract IChing {
 	function line(string _question, uint _lineHeight) internal constant returns (uint) {
 		uint randomNumber = uint(keccak256(block.number, msg.sender, _question, _lineHeight)) % 16;
 		if (randomNumber == 0) {
-			return 0;
+			return 6;
 		} else if (randomNumber > 0 && randomNumber <= 7) {
-			return 1;
+			return 8;
 		} else if (randomNumber > 7 && randomNumber <= 10) {
-			return 2;
+			return 9;
 		} else {
-			return 3;
+			return 7;
 		}
 	}
 
